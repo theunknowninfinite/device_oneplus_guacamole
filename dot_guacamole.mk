838946dc0a37cab3ec74e2905bdc9923a46c3aaf
infinity <lauraangels007@gmail.com>
2021-05-04 21:06:28 +0530

    Dot os bring-up (forgot git add lol)

---
+++ dot_guacamole.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
+$(call inherit-product, device/oneplus/guacamole/device.mk)
+
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1440
+
+WITH_GAPPS := true
+TARGET_SUPPORTS_BLUR := true
+EXTRA_FOD_ANIMATIONS := true
+
+
+
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := dot_guacamole
+PRODUCT_DEVICE := guacamole
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OnePlus 7 Pro
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+
+# Build info
+BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/1909110008:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=OnePlus7Pro \
+    PRODUCT_NAME=OnePlus7Pro \
+    PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
